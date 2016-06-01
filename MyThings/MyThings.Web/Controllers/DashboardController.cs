bcd217aae8eeeb82ccebdcbb0b5c25b2a0119f9c
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-01 16:51:52 +0200

    Cleanup Code
    Standard ASP code removed

---
+++ MyThings/MyThings.Web/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyThings.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
