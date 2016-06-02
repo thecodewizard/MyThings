6537061fa9d6014a9d85889e1a89ec7d8dd9eb5b
Bruno Vercruysse <bruno.vercruysse@student.howest.be>
2016-06-03 00:52:37 +0200

    Custom fonts
    Added custom fonts with Sass

--- MyThings/MyThings.Web/App_Start/BundleConfig.cs
+++ MyThings/MyThings.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace MyThings.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Styles/css").Include(
+                       "~/Styles/Bundled.css"));
         }
     }
 }
