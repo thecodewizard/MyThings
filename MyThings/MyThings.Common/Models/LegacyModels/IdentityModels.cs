6d6f327ad0d8be4e3541fa0583af1a372e4e56b5
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-03 15:49:20 +0200

    Models ASP Moved To Common
    The ASP Models are moved to the common project and the namespaces have
    been adapted

--- MyThings/MyThings.Common/Models/LegacyModels/IdentityModels.cs
+++ MyThings/MyThings.Common/Models/LegacyModels/IdentityModels.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace MyThings.Web.Models
+namespace MyThings.Common.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
