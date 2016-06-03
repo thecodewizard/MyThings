c2d627eaa0d7a9df669d3352e34a40dc7bbb7cb5
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-03 15:23:11 +0200

    References Adjusted & EF Introduced

--- MyThings/MyThings.Common/Repositories/GenericRepository.cs
+++ MyThings/MyThings.Common/Repositories/GenericRepository.cs
@@ -2,68 +2,67 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using MyThings.Common.Repositories.Interfaces;
-using MyThings.Web.Models;
 
 namespace MyThings.Common.Repositories
 {
-    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
-    {
+    //public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    //{
 
-        internal ApplicationDbContext context;
-        internal DbSet<TEntity> dbSet;
+    //    internal ApplicationDbContext context;
+    //    internal DbSet<TEntity> dbSet;
 
 
-        public GenericRepository()
-        {
-            this.context = new ApplicationDbContext();
-            this.dbSet = context.Set<TEntity>();
-        }
+    //    public GenericRepository()
+    //    {
+    //        this.context = new ApplicationDbContext();
+    //        this.dbSet = context.Set<TEntity>();
+    //    }
 
-        public GenericRepository(ApplicationDbContext context)
-        {
-            this.context = context;
-            this.dbSet = context.Set<TEntity>();
-        }
+    //    public GenericRepository(ApplicationDbContext context)
+    //    {
+    //        this.context = context;
+    //        this.dbSet = context.Set<TEntity>();
+    //    }
 
-        public virtual IEnumerable<TEntity> All()
-        {
-            return dbSet;
-        }
+    //    public virtual IEnumerable<TEntity> All()
+    //    {
+    //        return dbSet;
+    //    }
 
-        public virtual TEntity GetByID(object id)
-        {
-            return dbSet.Find(id);
-        }
+    //    public virtual TEntity GetByID(object id)
+    //    {
+    //        return dbSet.Find(id);
+    //    }
 
-        public virtual TEntity Insert(TEntity entity)
-        {
-            return dbSet.Add(entity);
-        }
+    //    public virtual TEntity Insert(TEntity entity)
+    //    {
+    //        return dbSet.Add(entity);
+    //    }
 
-        public virtual void Delete(object id)
-        {
-            TEntity entityToDelete = dbSet.Find(id);
-            Delete(entityToDelete);
-        }
+    //    public virtual void Delete(object id)
+    //    {
+    //        TEntity entityToDelete = dbSet.Find(id);
+    //        Delete(entityToDelete);
+    //    }
 
-        public virtual void Delete(TEntity entityToDelete)
-        {
-            if (context.Entry(entityToDelete).State == EntityState.Detached)
-            {
-                dbSet.Attach(entityToDelete);
-            }
-            dbSet.Remove(entityToDelete);
-        }
+    //    public virtual void Delete(TEntity entityToDelete)
+    //    {
+    //        if (context.Entry(entityToDelete).State == EntityState.Detached)
+    //        {
+    //            dbSet.Attach(entityToDelete);
+    //        }
+    //        dbSet.Remove(entityToDelete);
+    //    }
 
-        public virtual void Update(TEntity entityToUpdate)
-        {
-            dbSet.Attach(entityToUpdate);
-            context.Entry(entityToUpdate).State = EntityState.Modified;
-        }
+    //    public virtual void Update(TEntity entityToUpdate)
+    //    {
+    //        dbSet.Attach(entityToUpdate);
+    //        context.Entry(entityToUpdate).State = EntityState.Modified;
+    //    }
 
-        public virtual void SaveChanges()
-        {
-            context.SaveChanges();
-        }
-    }
+    //    public virtual void SaveChanges()
+    //    {
+    //        context.SaveChanges();
+    //    }
+    //}
 }
--- MyThings/MyThings.Common/Repositories/Interfaces/IGenericRepository.cs
+++ MyThings/MyThings.Common/Repositories/Interfaces/IGenericRepository.cs
@@ -2,15 +2,15 @@
 
 namespace MyThings.Common.Repositories.Interfaces
 {
-    public interface IGenericRepository<TEntity>
-        where TEntity : class
-    {
-        IEnumerable<TEntity> All();
-        void Delete(object id);
-        void Delete(TEntity entityToDelete);
-        TEntity GetByID(object id);
-        TEntity Insert(TEntity entity);
-        void Update(TEntity entityToUpdate);
-        void SaveChanges();
-    }
+    //public interface IGenericRepository<TEntity>
+    //    where TEntity : class
+    //{
+    //    IEnumerable<TEntity> All();
+    //    void Delete(object id);
+    //    void Delete(TEntity entityToDelete);
+    //    TEntity GetByID(object id);
+    //    TEntity Insert(TEntity entity);
+    //    void Update(TEntity entityToUpdate);
+    //    void SaveChanges();
+    //}
 }
