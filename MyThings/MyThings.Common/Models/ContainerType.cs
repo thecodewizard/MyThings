384b8d1737483b1f7ef8d714c0cbbbc87d80ba2d
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-03 19:48:34 +0200

    Entity Framework Gepruts Vrijdagmiddag
    Opzetten Entity Framework

---
+++ MyThings/MyThings.Common/Models/ContainerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThings.Common.Models
+{
+    public class ContainerType
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
