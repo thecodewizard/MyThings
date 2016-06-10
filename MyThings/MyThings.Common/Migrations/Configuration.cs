dd7d8bc7057a4df54c095169fffcc3ae2c100e90
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-10 14:53:19 +0200

    Company Added to User

--- MyThings/MyThings.Common/Migrations/Configuration.cs
+++ MyThings/MyThings.Common/Migrations/Configuration.cs
@@ -66,7 +66,7 @@ namespace MyThings.Common.Migrations
             context.Timeholder.Add(holder);
 
             //Generate dummy data for the database
-            GenerateDummyData(appUser);
+            //GenerateDummyData(appUser);
         }
 
         #region DummyDataGenerator
