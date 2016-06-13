9906f856c5712ca22cf179b79676dd1fb270b82c
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-13 15:48:57 +0200

    Late Change

--- MyThings/MyThings.Web/Controllers/TestController.cs
+++ MyThings/MyThings.Web/Controllers/TestController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using MyThings.Common.Helpers;
 using MyThings.Common.Models.FrontEndModels;
 using Newtonsoft.Json;
 
@@ -61,30 +62,31 @@ namespace MyThings.Web.Controllers
                 //This will result in the user specific custom homepage
                 ApplicationUser user = UserManager.FindByName(User.Identity.Name);
 
-                List<Pin> pins = _pinRepository.GetPinsForUser(user.Id);
-                foreach(Pin pin in pins) _pinRepository.Delete(pin);
-                _pinRepository.SaveChanges();
+                List<Tile> tiles = new List<Tile>();
+                List<Sensor> sensors = _sensorRepository.GetSensors(null, true);
+                foreach (Sensor sensor in sensors)
+                {
+                    tiles.Add(new Tile() {Pin = new Pin()
+                    {
+                        SavedId = sensor.Id,
+                        SavedType = PinType.Sensor,
+                        UserId = user.Id
+                    } });
 
-                List<Sensor> allSensors = _sensorRepository.GetSensors();
-                List<Container> allContainers = _containerRepository.GetContainers();
-                foreach (Sensor sensor in allSensors)
-                {
-                    Pin pin = new Pin();
-                    pin.SavedId = sensor.Id;
-                    pin.SavedType = PinType.Sensor;
-                    pin.UserId = user.Id;
-                    _pinRepository.Insert(pin);
+                    foreach (Container container in sensor.Containers)
+                    {
+                        tiles.Add(new Tile()
+                        {
+                            Pin = new Pin()
+                            {
+                                SavedId = container.Id,
+                                SavedType = PinType.Container,
+                                UserId = user.Id
+                            }
+                        });
+                    }
                 }
-                _pinRepository.SaveChanges();
-                foreach (Container container in allContainers)
-                {
-                    Pin pin = new Pin();
-                    pin.SavedId = container.Id;
-                    pin.SavedType = PinType.Container;
-                    pin.UserId = user.Id;
-                    _pinRepository.Insert(pin);
-                }
-                _pinRepository.SaveChanges();
+                _pinRepository.UpdateGridsterJson(user.Id, GridsterHelper.TileListToJson(tiles));
             }
 
             return View();
