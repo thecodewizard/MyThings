40fc01f125419877eb9ddc8ae4dc2a496181b9cf
Esteban Denis <esteban.denis.ed@gmail.com>
2016-06-16 17:42:32 +0200

    Sensordetailpage made

--- MyThings/MyThings.Web/Controllers/DashboardController.cs
+++ MyThings/MyThings.Web/Controllers/DashboardController.cs
@@ -177,7 +177,7 @@ namespace MyThings.Web.Controllers
 
             //Check navigation
             List<Tile> navTiles = (from t in tiles where t.Pin.SavedType == PinType.FixedNavigation select t).ToList();
-            if (navTiles.Count() < 6 && navTiles.Any()) //If the count isn't correct, delete all the pins
+            if (navTiles.Count() != 4 && navTiles.Any()) //If the count isn't correct, delete all the pins
             {
                 foreach (Tile navTile in navTiles) tiles.Remove(navTile);
             }
